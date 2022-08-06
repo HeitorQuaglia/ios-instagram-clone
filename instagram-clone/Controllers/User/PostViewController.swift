338dcac090e8e4e8acca8e4ca376414ba3f6410c
Heitor Quaglia <heitor.quaglia@digitalrepublic.com.br>
2022-08-06 11:14:42 -0300

    User Controllers

---
+++ instagram-clone/Controllers/User/PostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PostViewController.swift
+//  instagram-clone
+//
+//  Created by Heitor Quaglia on 06/08/22.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
