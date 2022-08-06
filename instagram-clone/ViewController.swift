a73e63b7b5d502870a1c3e7fd7d95bd639f8f6a8
Heitor Quaglia <heitor.quaglia@digitalrepublic.com.br>
2022-08-06 02:09:37 -0300

    Initial Commit

---
+++ instagram-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram-clone
+//
+//  Created by Heitor Quaglia on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
