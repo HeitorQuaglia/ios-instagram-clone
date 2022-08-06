bf354047f743330f23d21d02f3e5644e3e6a84f4
Heitor Quaglia <heitor.quaglia@digitalrepublic.com.br>
2022-08-06 02:27:16 -0300

    Onboarding tabs

---
+++ instagram-clone/Controllers/Onboard/RegistrationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationViewController.swift
+//  instagram-clone
+//
+//  Created by Heitor Quaglia on 06/08/22.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
